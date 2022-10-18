240952ec0ebc85c9e430d1f5666fca4a32675aba
Mark Richmond <77017509+rich20053@users.noreply.github.com>
2022-10-17 22:38:00 -0600

    Week 6 V2

--- week6/ToDoApp/javascript/ToDos.js
+++ week6/ToDoApp/javascript/ToDos.js
@@ -1,6 +1,27 @@
-// To Dos javascript file
-import {Hikes} from "./javascript/hikes.js";
 
-const myHike = new Hikes;
-console.log(myHike);
-myHike.addHikeListener();
+class Task {
+    constructor(taskName) {
+        const name = taskName;
+        const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+        const id = new Date().toLocaleDateString('en-US', options);
+        var complete = false;
+    }
+}
+
+const taskList = [];
+
+function addIt() {
+    var taskName = document.getElementById("newItem").value; 
+    const thisTask = new Task(taskName);
+    taskList.push(thisTask);
+    console.log(taskList);
+    console.log(thisTask);
+    console.log(taskName);
+    const screenList = document.getElementById("taskList");
+    const item = document.createElement("li");
+    item.setAttribute('class', "listItem");
+    item.innerHTML = `<input type="checkbox">
+    <label>${taskName}</label>
+    <button onclick="deleteIt()">X</button>`;
+    screenList.appendChild(item);
+}

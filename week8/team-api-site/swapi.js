a7fa59d114f95b7a8b6201fae5a37a2feaf41e5b
Mark Richmond <77017509+rich20053@users.noreply.github.com>
2022-11-01 22:47:31 -0600

    Week 8 V1

---
+++ week8/team-api-site/swapi.js
@@ -0,0 +1,115 @@
+
+const peopleButton = document.getElementById('people');
+const shipsButton = document.getElementById('ships');
+const outputUL = document.getElementById('output');
+const prevButton = document.getElementById('prevSet');
+const nextButton = document.getElementById('nextSet');
+var currentSet = "";
+var nextURL = "";
+var prevURL = "";
+
+const peopleURL = 'https://swapi.dev/api/people/';
+const shipsURL = 'https://swapi.dev/api/starships/';
+
+peopleButton.addEventListener('click', () => {
+    fetch(peopleURL)
+    .then( response => {
+        outputUL.innerHTML = 'Waiting for response...';
+        if(response.ok) {
+            return response;
+        }
+            throw Error(response.statusText);
+        }
+    )
+    .then( response => response.json() )
+    .then( data => {
+        nextURL = data.next;
+        prevURL = data.previous;
+        var itemArray = data.results;
+        outputUL.innerHTML = '';
+        for (var i=0; i<itemArray.length; i++) {
+            displayItem(itemArray[i]);
+        }
+    })
+    .catch( error => console.log('There was an error:', error))
+},false);
+
+shipsButton.addEventListener('click', () => {
+    currentSet = "ships";
+    fetch(shipsURL)
+    .then( response => {
+        outputUL.innerHTML = 'Waiting for response...';
+        if(response.ok) {
+            return response;
+        }
+        throw Error(response.statusText);
+        }
+    )
+    .then( response => response.json() )
+    .then( data => {
+        nextURL = data.next;
+        prevURL = data.previous;
+        var itemArray = data.results;
+        outputUL.innerHTML = '';
+        for (var i=0; i<itemArray.length; i++) {
+            displayItem(itemArray[i]);
+        }
+    })
+    .catch( error => console.log('There was an error:', error))
+},false);
+
+function displayItem(itemData) {
+    const item = document.createElement("li");
+    item.innerHTML = itemData.name;
+    item.setAttribute("class", "item");
+    outputUL.appendChild(item);
+}
+
+prevButton.addEventListener('click', () => {
+    if (prevURL!=null) {
+        fetch(prevURL)
+        .then( response => {
+            outputUL.innerHTML = 'Waiting for response...';
+            if(response.ok) {
+                return response;
+            }
+            throw Error(response.statusText);
+            }
+        )
+        .then( response => response.json() )
+        .then( data => {
+            nextURL = data.next;
+            prevURL = data.previous;
+            var thisArray = data.results;
+            outputUL.innerHTML = '';
+            for (var i=0; i<thisArray.length; i++) {
+                displayItem(thisArray[i]);
+            }
+        })
+        .catch( error => console.log('There was an error:', error))
+    }
+},false);
+
+nextButton.addEventListener('click', () => {
+    if (nextURL!=null) {
+        fetch(nextURL)
+        .then( response => {
+            outputUL.innerHTML = 'Waiting for response...';
+            if(response.ok) {
+                return response;
+            }
+            throw Error(response.statusText);
+        })
+        .then( response => response.json() )
+        .then( data => {
+            nextURL = data.next;
+            prevURL = data.previous;
+            var thisArray = data.results;
+            outputUL.innerHTML = '';
+            for (var i=0; i<thisArray.length; i++) {
+                displayItem(thisArray[i]);
+            }
+        })
+        .catch( error => console.log('There was an error:', error))
+    }
+},false);

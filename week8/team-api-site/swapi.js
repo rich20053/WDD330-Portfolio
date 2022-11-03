c23b6ff9ea3ee98c2836b8226d0d2f84e4cca13c
Mark Richmond <77017509+rich20053@users.noreply.github.com>
2022-11-03 17:01:46 -0600

    Week 8 V2

--- week8/team-api-site/swapi.js
+++ week8/team-api-site/swapi.js
@@ -12,6 +12,7 @@ const peopleURL = 'https://swapi.dev/api/people/';
 const shipsURL = 'https://swapi.dev/api/starships/';
 
 peopleButton.addEventListener('click', () => {
+    currentSet = "people";
     fetch(peopleURL)
     .then( response => {
         outputUL.innerHTML = 'Waiting for response...';
@@ -62,6 +63,14 @@ function displayItem(itemData) {
     const item = document.createElement("li");
     item.innerHTML = itemData.name;
     item.setAttribute("class", "item");
+
+    item.addEventListener('touchend', () => {
+        renderThisDetail(itemData);
+      });
+    item.addEventListener('click', () => {
+        renderThisDetail(itemData);
+      });
+
     outputUL.appendChild(item);
 }
 
@@ -113,3 +122,72 @@ nextButton.addEventListener('click', () => {
         .catch( error => console.log('There was an error:', error))
     }
 },false);
+
+function renderThisDetail(passedItemData) {
+    if (currentSet == "people") {
+        var outputUL = document.querySelector("ul");
+        var child = outputUL.lastElementChild;
+        // Remove all current tasks from HTML
+        while (child) {
+            outputUL.removeChild(child);
+            child = outputUL.lastElementChild;
+        }
+        outputUL.textContent = `${passedItemData.name}`;
+        outputUL.setAttribute("class", "ulhead");
+        var item = document.createElement("li");
+        item.innerHTML = `Born: ${passedItemData.birth_year}`;
+        outputUL.appendChild(item);
+        item = document.createElement("li");
+        item.innerHTML = `Gender: ${passedItemData.gender}`;
+        outputUL.appendChild(item);
+        item = document.createElement("li");
+        item.innerHTML = `Hair Color: ${passedItemData.hair_color}`;
+        outputUL.appendChild(item);
+        item = document.createElement("li");
+        item.innerHTML = `Eye Color: ${passedItemData.eye_color}`;
+        outputUL.appendChild(item);
+        item = document.createElement("li");
+        item.innerHTML = `Height: ${passedItemData.height}`;
+        outputUL.appendChild(item);
+    }
+    else if (currentSet == "ships") {
+        var outputUL = document.querySelector("ul");
+        var child = outputUL.lastElementChild;
+        // Remove all current tasks from HTML
+        while (child) {
+            outputUL.removeChild(child);
+            child = outputUL.lastElementChild;
+        }
+        outputUL.textContent = `${passedItemData.name}`;
+        outputUL.setAttribute("class", "ulhead");
+        var item = document.createElement("li");
+        item.innerHTML = `Model: ${passedItemData.model}`;
+        outputUL.appendChild(item);
+        item = document.createElement("li");
+        item.innerHTML = `Hyperdrive: ${passedItemData.hyperdrive_rating}`;
+        outputUL.appendChild(item);
+        item = document.createElement("li");
+        item.innerHTML = `Crew: ${passedItemData.crew}`;
+        outputUL.appendChild(item);
+        item = document.createElement("li");
+        item.innerHTML = `Length: ${passedItemData.length}`;
+        outputUL.appendChild(item);
+        item = document.createElement("li");
+        item.innerHTML = `Cargo: ${passedItemData.cargo_capacity}`;
+        outputUL.appendChild(item);
+
+    }
+    else {
+        //not found
+    }
+}
+
+
+
+
+
+
+
+
+
+
